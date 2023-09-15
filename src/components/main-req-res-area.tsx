879ab7ead1fbc38d72c877e12c1c04f1206348ba
Lucas Machado <lukaasmachado143@gmail.com>
2023-09-15 01:31:42 -0300

    feat: refactor request response area.

---
+++ src/components/main-req-res-area.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Textarea } from "./ui/textarea";
+
+export function MainReqResArea() {
+    const [prompt, setPrompt] = useState<string>("")
+    const [resultAi, setResultAi] = useState<string>("")
+    return (
+        <div className="flex flex-col flex-1 gap-4">
+            <div className="grid grid-rows-2 gap-4 flex-1">
+                <Textarea
+                    value={prompt}
+                    onChange={(event) => {
+                        setPrompt(event.target.value)
+                    }}
+                    className="resize-none p-4 leading-relaxed"
+                    placeholder="Inclua o prompt para a IA ..."
+                />
+                <Textarea
+                    value={resultAi}
+                    className="resize-none p-4 leading-relaxed"
+                    placeholder="Resultado gerado pela IA ..."
+                    readOnly
+                />
+            </div>
+            <p className="text-sm text-muted-foreground">
+                Lembre-se: você pode utilizar a variável <code className="text-violet-400">{"{transcription}"}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+            </p>
+        </div>
+
+    )
+}
