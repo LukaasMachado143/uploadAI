acc3c243916ba39b7eddd7a4a92e674354152762
Lucas Machado <lukaasmachado143@gmail.com>
2023-09-14 23:13:17 -0300

    feat: Finished integration video form

--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,6 +6,7 @@ import { Button } from "./ui/button";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
 import { getFFmpeg } from "@/lib/ffmpeg"
 import { fetchFile } from "@ffmpeg/util"
+import { api } from "@/lib/axios";
 export function VideoInputForm() {
     const [videoFile, setVideoFile] = useState<File | null>(null)
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
@@ -62,6 +63,20 @@ export function VideoInputForm() {
         // converter vídeo em aúdio
         const audioFile = await convertVideoToAudio(videoFile)
         console.log(audioFile, prompt)
+
+        const data = new FormData()
+        data.append('file', audioFile)
+
+        const response = await api.post("/videos", data)
+
+        console.log(response.data)
+
+        const videoId = response.data.id
+
+        await api.post(`/videos/${videoId}/transcription`, { prompt })
+
+        console.log('Finalizou')
+
     }
 
     const previewUrl = useMemo(() => {
